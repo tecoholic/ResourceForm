4b62418271acab4b594075a07f33e2b816353b36
Arunmozhi <arun@arunmozhi.in>
2020-04-03 17:00:59 +0530

    fix map zoom reset

--- src/components/LocationSelector.jsx
+++ src/components/LocationSelector.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 
 function LocationSelector(props) {
+  const [zoom, setZoom] = useState(13);
   const markermoved = (e) => {
     const { lat, lng } = e.target._latlng;
     props.updateLocation([lat, lng]);
   };
 
+  const updateZoomLevel = (e) => {
+    setZoom(e.target._zoom);
+  };
+
   return (
     <div>
-    <Map center={props.currentLocation} zoom={13} className="Map">
+    <Map center={props.currentLocation} zoom={zoom} className="Map" onzoomend={updateZoomLevel}>
       <TileLayer
         url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
         attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
